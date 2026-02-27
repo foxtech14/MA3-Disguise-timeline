7d9d59e1ff0b5873ee820e037f420cb1ad607f45
Michael Fox <foxtech14@gmail.com>
2026-02-27 22:04:22 +0000

    v2 updates to make it a more distributable solution

--- source/ma3_disguise_timeline.lua
+++ source/ma3_disguise_timeline.lua
@@ -1,47 +1,73 @@
--- BBLX Disguise Timeline Wizard v1
+-- BBLX Disguise Timeline Wizard v2
 -- By Michael Fox
--- 17-04-24
-
--- ****************************************************************
--- USER CONFIG AREA - ONLY EDIT THIS BIT
--- ****************************************************************
-
-local fixture = 7001 -- Timeline Fixture
-
+-- 27-02-26
 
 -- ****************************************************************
 -- local plugin variables
 -- ****************************************************************
 
-local C = Cmd
-local E = Echo
-local MB = MessageBox
+-- global function cache
+local C = Cmd;
+local E = Echo;
+local MB = MessageBox;
+
+-- function lookup table
+local functions = {};
 
 
 -- ****************************************************************
--- clamp(number, number, number) : number
+-- macro creation function
 -- ****************************************************************
-function clamp(input, min, max)
-    local ErrorString = "clamp(number:input, number:min, number:max) "
-    assert(type(input) == "number", ErrorString .. "- input, must be a number")
-    assert(type(min) == "number", ErrorString .. "- min, must be a number")
-    assert(type(max) == "number", ErrorString .. "- max, must be a number")
-    assert(min <= max, ErrorString .. "- min must be less or equal to max")
-    local i = input
-    if i < min then i = min end
-    if i > max then i = max end
-    return i
+
+local function createMacros()
+    -- input dialog for user input for cue number
+    local macroNum = MB({
+        title = "Start Macro number?",
+        commands = {{value = 1, name = "Ok"}},
+        inputs = {{name = "Macro", whiteFilter = "0123456789"}},
+        backColor = "Global.Default",
+        icon = "logo_small",
+    });
+
+    -- convert user input
+    local num = clamp(tonumber(macroNum.inputs['Macro']), 1, 9999);
+    local cmd1 = 'Call Plugin "BBLX Disguise Cue Trigger" "create"';
+    local cmd2 = 'Call Plugin "BBLX Disguise Cue Trigger" "delete"';
+    local cmd3 = 'Call Plugin "BBLX Disguise Cue Trigger" "setfixture"';
+
+    C(string.format("Store Macro %s.1", num));
+    C(string.format("Set Macro %s.1 Property \"Command\" \"%s\"", num, cmd1));
+    C(string.format("Label Macro %s \"%s\"", num, "Store Disgusie Trigger"));
+
+    num = num + 1;
+    C(string.format("Store Macro %s.1", num));
+    C(string.format("Set Macro %s.1 Property \"Command\" \"%s\"", num, cmd2));
+    C(string.format("Label Macro %s \"%s\"", num, "Delete Disgusie Trigger"));
+
+    num = num + 1;
+    C(string.format("Store Macro %s.1", num));
+    C(string.format("Set Macro %s.1 Property \"Command\" \"%s\"", num, cmd3));
+    C(string.format("Label Macro %s \"%s\"", num, "Set Disguise Fixture Number"));
 end
 
-
 -- ****************************************************************
--- main function
+-- cue input function
 -- ****************************************************************
 
-local function main()
-    E("Disguise Wizard Started")
-    local qNum, padNum, cueXX, cueYY, cueZZ
+-- clamp(number, number, number) : number
+local function clamp(input, min, max)
+    local ErrorString = "clamp(number:input, number:min, number:max) ";
+    assert(type(input) == "number", ErrorString .. "- input, must be a number");
+    assert(type(min) == "number", ErrorString .. "- min, must be a number");
+    assert(type(max) == "number", ErrorString .. "- max, must be a number");
+    assert(min <= max, ErrorString .. "- min must be less or equal to max");
+    local i = input;
+    if i < min then i = min end
+    if i > max then i = max end
+    return i;
+end
 
+local function getQ()
     -- input dialog for user input for cue number
     local inputCue = MB({
         title = "What Cue number?",
@@ -52,36 +78,64 @@ local function main()
     });
 
     -- convert user input
-    qNum = clamp(tonumber(inputCue.inputs['Q']), 0.01, 9999.99)
-    padNum = qNum + 10000.001
+    local num = clamp(tonumber(inputCue.inputs['Q']), 0.01, 9999.99);
+    return num;
+end
 
-    E(string.format("Cue Number: %s", qNum))
 
+-- ****************************************************************
+-- cue function
+-- ****************************************************************
+
+functions['create'] = function()
+    local qNum, padNum, cueXX, cueYY, cueZZ;
+    local fixture = GetVar(UserVars(),'d3fixture');
+
+    qNum = getQ();
+    padNum = qNum + 10000.001;
     -- extract relevant numbers
-    cueXX = string.sub(padNum, -8,-7)
-    cueYY = string.sub(padNum, -6,-5)
-    cueZZ = string.sub(padNum, -3,-2)
+    cueXX = string.sub(padNum, -8,-7);
+    cueYY = string.sub(padNum, -6,-5);
+    cueZZ = string.sub(padNum, -3,-2);
 
-    E(string.format("> XX: %s", cueXX))
-    E(string.format("> YY: %s", cueYY))
-    E(string.format("> ZZ: %s", cueZZ))
+    E(string.format("Cue Number: %s", qNum));
+    E(string.format("> XX: %s", cueXX));
+    E(string.format("> YY: %s", cueYY));
+    E(string.format("> ZZ: %s", cueZZ));
 
-    C("BlindEdit On")
-    C("ClearAll")
+    C("ClearAll");
 
     -- put fixture at values
-    C(string.format("Fixture %s", fixture))
-    C(string.format("Attribute 'cueXX' at %s", cueXX))
-    C(string.format("Attribute 'cueYY' at %s", cueYY))
-    C(string.format("Attribute 'cueZZ' at %s", cueZZ))
+    C(string.format("Fixture %s", fixture));
+    C(string.format("Attribute 'cueXX' at %s", cueXX));
+    C(string.format("Attribute 'cueYY' at %s", cueYY));
+    C(string.format("Attribute 'cueZZ' at %s", cueZZ));
 
-    C(string.format("Store Cue %s Part 71 Time 0 /m /nc", qNum))
-    C(string.format("Label Cue %s Part 71 'Video'", qNum))
+    C(string.format("Store Cue %s Part 71 Time 0 /m /nc", qNum));
+    C(string.format("Label Cue %s Part 71 'Video'", qNum));
+    C(string.format("Assign Appearance 'd3' at Cue %s Part 71", qNum));
 
     -- clear all values
-    C("ClearAll")
-    C("BlindEdit Off")
-    E("Wizard Closed")
+    C("ClearAll");
+end
+
+functions['delete'] = function()
+    local qNum = getQ();
+    C(string.format("Delete Cue %s Part 71 /nc", qNum));
+end
+
+-- ****************************************************************
+-- caller function
+-- ****************************************************************
+
+local function main(display,argument)
+    E("Disguise Q Wizard");
+    if argument then
+        functions[spiltString(argument,",",1)]();
+    else
+        functions['create']();
+    end
+    E("Complete");
 end
 
 return main;
